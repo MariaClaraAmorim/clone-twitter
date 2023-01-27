f23214649a02fee7943268f2c51928fe3dca9d45
maria.amorim <maria.amorim@dantaseletro.com.br>
2023-01-27 16:23:35 -0300

    feat: add configs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: "@assets",
+        replacement: resolve("src/assets"),
+      },
+      {
+        find: "@common",
+        replacement: resolve("src/common"),
+      },
+      {
+        find: "@components",
+        replacement: resolve("src/components"),
+      },
+      {
+        find: "@config",
+        replacement: resolve("src/config"),
+      },
+      {
+        find: "@contexts",
+        replacement: resolve("src/contexts"),
+      },
+      {
+        find: "@hooks",
+        replacement: resolve("src/hooks"),
+      },
+      {
+        find: "@services",
+        replacement: resolve("src/services"),
+      },
+      {
+        find: "@styles",
+        replacement: resolve("src/styles"),
+      },
+      {
+        find: "@",
+        replacement: resolve("src"),
+      },
+    ],
+  },
+});
