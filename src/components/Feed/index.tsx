6dff79294d486bfb38d70594317e1aefbe00bf85
maria.amorim <maria.amorim@dantaseletro.com.br>
2023-02-01 16:57:04 -0300

    refactor: busca dados reais

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -1,15 +1,35 @@
-import React from "react";
+import { Tweet as TweetType } from "@common/interface/Post";
+import { useUserContext } from "@contexts/UserContext";
+import { getTweetsByUserId } from "@services/tweets";
+import React, { useEffect, useState } from "react";
 
-import {Tweet} from "../Tweet";
+import { Tweet } from "../Tweet";
 
 import { Container, Tab, Tweets } from "./styles";
 
 function Feed() {
+  const { user } = useUserContext();
+
+  const [tweets, setTweets] = useState<TweetType[]>([]);
+
+  async function getMyTweets() {
+    const userId = user?.userId ?? "";
+    const response = await getTweetsByUserId(userId);
+
+    setTweets(response);
+  }
+
+  useEffect(() => {
+    getMyTweets();
+  }, []);
+
   return (
     <Container>
       <Tab> Tweets </Tab>
       <Tweets>
-        <Tweet />
+        {tweets.map((tweet, index) => {
+          return <Tweet tweet={tweet} key={index} />;
+        })}
       </Tweets>
     </Container>
   );
